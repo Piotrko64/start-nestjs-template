dc7177cfd5b75ce1870a73d2a6af18665fd740f6
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-11-28 17:22:49 +0100

    Add simple webSocket

--- src/web-socket/gateway.ts
+++ src/web-socket/gateway.ts
@@ -2,10 +2,31 @@ import {
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { OnModuleInit } from '@nestjs/common/interfaces';
 
 @WebSocketGateway()
-export class GateWay {
-  @SubscribeMessage('newMEssage')
-  onNewMessage(@MessageBody() body: any) {}
+export class GateWay implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log(socket.id);
+      console.log('connect');
+    });
+  }
+
+  @SubscribeMessage('newMessage')
+  onNewMessage(@MessageBody() body: any) {
+    // Look postmanExample.png in /assets
+    console.log(body);
+    // Remember about 'onMessage' event in postman
+    this.server.emit('onMessage', {
+      msg: 'new',
+      content: body,
+    });
+  }
 }
