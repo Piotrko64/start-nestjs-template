5dfa7ac93b7e0d4849af17f269f795b7d08d74ce
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-12-09 17:59:53 +0100

    TEST eslint

--- src/guards/peter-name/peter-name.guard.spec.ts
+++ src/guards/peter-name/peter-name.guard.spec.ts
@@ -1,5 +1,5 @@
 import { PeterNameGuard } from './peter-name.guard';
-import { of } from 'rxjs';
+import { of, from } from 'rxjs';
 
 describe('PeterNameGuard', () => {
   it('should be defined', () => {
