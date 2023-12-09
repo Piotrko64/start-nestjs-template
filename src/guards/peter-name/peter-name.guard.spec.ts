1259576452fcdccbf3b36700e7a970e7268d8bcf
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2023-12-09 17:57:27 +0100

    TEST

--- src/guards/peter-name/peter-name.guard.spec.ts
+++ src/guards/peter-name/peter-name.guard.spec.ts
@@ -1,4 +1,6 @@
 import { PeterNameGuard } from './peter-name.guard';
+import { of } from 'rxjs';
+
 describe('PeterNameGuard', () => {
   it('should be defined', () => {
     expect(new PeterNameGuard()).toBeDefined();
